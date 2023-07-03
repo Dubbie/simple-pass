65772c2251914a5c83b44c293bf35dc18204cbb0
Mihó Dániel <miho.daniel@mdy.hu>
2023-07-03 23:32:33 +0200

    Fixes active sidebar links

--- resources/js/Components/SidebarFolderLink.vue
+++ resources/js/Components/SidebarFolderLink.vue
@@ -11,7 +11,10 @@ const props = defineProps({
 </script>
 
 <template>
-    <SidebarLink :href="route('folders.show', folder)">
+    <SidebarLink
+        :href="route('folders.show', folder)"
+        :active="route().current('folders.show', folder)"
+    >
         <template #icon>
             <span
                 class="bg-gray-800 inline-flex items-center justify-center h-5 w-5 rounded border border-gray-700 text-gray-500 font-semibold text-[0.625rem]"
@@ -22,8 +25,8 @@ const props = defineProps({
         <span>{{ folder.name }}</span>
     </SidebarLink>
 
-    <span v-if="folder.sub_folders.length > 0" class="block ml-4">
-        <ul>
+    <span v-if="folder.sub_folders.length > 0" class="block ml-4 mt-1">
+        <ul class="space-y-1">
             <template v-for="subFolder in folder.sub_folders">
                 <li>
                     <SidebarFolderLink :folder="subFolder" />
