014c625a1103e86fb16f3a8cc3e25d743065b7a5
Mihó Dániel <miho.daniel@mdy.hu>
2023-07-03 23:15:11 +0200

    Implements folder component

---
+++ resources/js/Components/SidebarFolderLink.vue
@@ -0,0 +1,34 @@
+<script setup>
+import { ref } from "vue";
+import SidebarLink from "@/Components/SidebarLink.vue";
+
+const props = defineProps({
+    folder: {
+        type: Object,
+        required: true,
+    },
+});
+</script>
+
+<template>
+    <SidebarLink :href="route('folders.show', folder)">
+        <template #icon>
+            <span
+                class="bg-gray-800 inline-flex items-center justify-center h-5 w-5 rounded border border-gray-700 text-gray-500 font-semibold text-[0.625rem]"
+            >
+                {{ folder.name.substr(0, 1) }}
+            </span>
+        </template>
+        <span>{{ folder.name }}</span>
+    </SidebarLink>
+
+    <span v-if="folder.sub_folders.length > 0" class="block ml-4">
+        <ul>
+            <template v-for="subFolder in folder.sub_folders">
+                <li>
+                    <SidebarFolderLink :folder="subFolder" />
+                </li>
+            </template>
+        </ul>
+    </span>
+</template>
--- resources/js/Components/SidebarMenu.vue
+++ resources/js/Components/SidebarMenu.vue
@@ -7,6 +7,7 @@ import SidebarHeader from "@/Components/Shared/SidebarHeader.vue";
 import LinkButton from "./Shared/LinkButton.vue";
 import NewFolderModal from "./NewFolderModal.vue";
 import { ref } from "vue";
+import SidebarFolderLink from "./SidebarFolderLink.vue";
 
 const refs = {
     showNewFolderModal: ref(false),
@@ -70,41 +71,7 @@ const handleNewFolder = () => {
                                 :key="folder.id"
                             >
                                 <li>
-                                    <SidebarLink
-                                        :href="route('folders.show', folder)"
-                                    >
-                                        <template #icon>
-                                            <span
-                                                class="bg-gray-800 inline-flex items-center justify-center h-5 w-5 rounded border border-gray-700 text-gray-500 font-semibold text-xs"
-                                            >
-                                                {{ folder.name.substr(0, 1) }}
-                                            </span>
-                                        </template>
-                                        <span>{{ folder.name }}</span>
-                                    </SidebarLink>
-
-                                    <span
-                                        v-if="folder.sub_folders.length > 0"
-                                        class="block ml-10"
-                                    >
-                                        <template
-                                            v-for="subFolder in folder.sub_folders"
-                                        >
-                                            <LinkButton
-                                                color="secondary"
-                                                class="text-xs"
-                                                :href="
-                                                    route(
-                                                        'folders.show',
-                                                        subFolder
-                                                    )
-                                                "
-                                                >{{
-                                                    subFolder.name
-                                                }}</LinkButton
-                                            >
-                                        </template>
-                                    </span>
+                                    <SidebarFolderLink :folder="folder" />
                                 </li>
                             </template>
                         </template>
