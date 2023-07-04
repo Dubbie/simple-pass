e7207f56133a8f249d8e0b3e2962d785d3060f89
Mihó Dániel <miho.daniel@mdy.hu>
2023-07-04 12:57:17 +0200

    Adds support to delete folder with entries

--- app/Models/Folder.php
+++ app/Models/Folder.php
@@ -20,6 +20,11 @@ class Folder extends Model
         return $this->hasMany(Folder::class, 'parent_id', 'id');
     }
 
+    public function parentFolder()
+    {
+        return $this->belongsTo(Folder::class, 'parent_id');
+    }
+
     public function scopeRoot()
     {
         return $this->whereNull('parent_id');
@@ -33,5 +38,15 @@ class Folder extends Model
     public function withEntries()
     {
         return Folder::with('entries')->find($this->id);
+    }
+
+    protected static function booted()
+    {
+        static::deleting(function ($folder) {
+            // Move the entries to the parent folder.
+            if ($folder->parent_id) {
+                $folder->entries()->update(['folder_id' => $folder->parent_id]);
+            }
+        });
     }
 }
