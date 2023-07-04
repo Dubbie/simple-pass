efa9caec8b47ba448dba1ce0f8e9e883841cf17e
Mihó Dániel <miho.daniel@mdy.hu>
2023-07-04 18:32:24 +0200

    Stops using dotted Tailwind classes for now

--- resources/js/Components/Shared/PrimaryButton.vue
+++ resources/js/Components/Shared/PrimaryButton.vue
@@ -20,9 +20,7 @@ const props = defineProps({
 const baseClasses =
     "rounded-md whitespace-nowrap bg-indigo-600 font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600";
 
-const sizeClasses = computed(() => {
-    return getSizeClasses(props.size);
-});
+const sizeClasses = getSizeClasses(props.size);
 </script>
 
 <template>
--- resources/js/common-functions.js
+++ resources/js/common-functions.js
@@ -2,7 +2,7 @@ const getSizeClasses = (size) => {
     return {
         xs: `px-2 py-1 text-xs`,
         sm: `px-2 py-1 text-sm`,
-        md: `px-2\.5 py-1\.5 text-sm`,
+        md: `px-3 py-2 text-sm`,
         lg: `px-3 py-2 text-sm`,
         xl: `px-3\.5 py-2\.5 text-base`,
     }[size];
