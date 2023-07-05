68048f534ae970dd094f790663fff1587b005cc3
Mihó Dániel <miho.daniel@mdy.hu>
2023-07-05 12:24:25 +0200

    Updates autofill style

--- resources/js/Components/Shared/TextInput.vue
+++ resources/js/Components/Shared/TextInput.vue
@@ -22,7 +22,7 @@ defineExpose({ focus: () => input.value.focus() });
 
 <template>
     <input
-        class="block rounded-md border-0 py-1.5 bg-gray-800 text-white shadow-sm ring-1 ring-inset ring-gray-700 placeholder:text-gray-500 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6"
+        class="block rounded-md border-0 py-1.5 bg-gray-800 text-white shadow-sm ring-1 ring-inset ring-gray-700 placeholder:text-gray-500 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6 autofill:shadow-[inset_0_0_0px_1000px_rgb(21,31,45)]"
         :value="modelValue"
         @input="$emit('update:modelValue', $event.target.value)"
         ref="input"
