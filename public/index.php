05271acea536d62704e9680a76bb57f2b3e3734b
Mihó Dániel <miho.daniel@mdy.hu>
2023-07-10 14:08:28 +0200

    fix: loading for production

--- public/index.php
+++ public/index.php
@@ -5,6 +5,7 @@ use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
 
+$bootstrap = $_SERVER['SERVER_NAME'] != '0.0.0.0' ? '/laravel' : ''; // Bootstrap for production
 /*
 |--------------------------------------------------------------------------
 | Check If The Application Is Under Maintenance
@@ -16,7 +17,7 @@ define('LARAVEL_START', microtime(true));
 |
 */
 
-if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
+if (file_exists($maintenance = __DIR__ . '/..' . $bootstrap . '/storage/framework/maintenance.php')) {
     require $maintenance;
 }
 
@@ -31,7 +32,7 @@ if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php'))
 |
 */
 
-require __DIR__.'/../vendor/autoload.php';
+require __DIR__ . '/..' . $bootstrap . '/vendor/autoload.php';
 
 /*
 |--------------------------------------------------------------------------
@@ -44,7 +45,7 @@ require __DIR__.'/../vendor/autoload.php';
 |
 */
 
-$app = require_once __DIR__.'/../bootstrap/app.php';
+$app = require_once __DIR__ . '/..' . $bootstrap . '/bootstrap/app.php';
 
 $kernel = $app->make(Kernel::class);
 
